9e160d31e00c24a09d9a295984140f461cba09f8
Pureschko <inna.shekina@gmail.com>
2024-11-07 12:50:42 +0100

    Fixed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,53 +16,33 @@ function Header() {
   return (
     <header className="header flex py-[3rem] px-[5rem] items-center bg-white">
   <div className="logo mr-auto">Pokemon</div>
-  <nav className="flex-1 flex justify-center">
-        <ul className="flex gap-8 items-center">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/favorites">My Favorites</Link>
-          </li>
-          <li>
-            <Link to="/leaderboard">Leaderboard</Link>
-          </li>
-        </ul>
-      </nav>
-      <div>
-        {user ? (
-          <div className="text-center">
-            <span className="text-gray-800 font-medium mr-4">
-              {user.firstName}
-            </span>
-            {/* <span className="text-gray-600 font-medium">, {user.country}</span> */}
-          </div>
-        ) : (
-          <button
-            onClick={handleLoginClick}
-            className="text-gray-800 font-medium mr-4"
-          >
-            Log in
-          </button>
-        )}
-      </div>
-      <div>
-        <Link
-          to="/battle"
-          className="bg-[#9B44E5] text-white rounded-[6px] py-[8px] px-[14px]"
-        >
-          Let&apos;s Fight!
-        </Link>
-      </div>
-      {isLoginPopupVisible && (
-        <div className="login-popup">
-          <div className="popup-content">
-            <LoginPopup onClose={handleClosePopup} />
-          </div>
-        </div>
+  <nav className="absolute left-1/2 transform -translate-x-1/2">
+      <ul className="flex gap-[2rem] items-center">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/favorites">My Favorites</Link></li>
+        <li><Link to="/leaderboard">Leaderboard</Link></li>
+      </ul>
+    </nav>
+    <div className="ml-auto flex items-center gap-4">
+      {user ? (
+        <span className="text-gray-800 font-medium mr-4">{user.firstName}</span>
+      ) : (
+        <button onClick={handleLoginClick} className="text-gray-800 font-medium">
+          Log in
+        </button>
       )}
-    </header>
-  );
+      <Link to="/battle" className="bg-[#9B44E5] text-white rounded-[6px] py-[8px] px-[14px]">
+        Let&apos;s Fight!
+      </Link>
+    </div>
+
+    {isLoginPopupVisible && (
+      <div className="login-popup">
+        <LoginPopup onClose={handleClosePopup} />
+      </div>
+    )}
+  </header>
+);
 }
 
 export default Header;
